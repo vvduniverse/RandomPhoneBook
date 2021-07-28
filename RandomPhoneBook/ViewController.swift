0a2b91fde0f390f55b5d4edb0ed01974300031e6
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-07-28 23:09:52 +0300

    Initial Commit

---
+++ RandomPhoneBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhoneBook
+//
+//  Created by Vahtee Boo on 28.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
